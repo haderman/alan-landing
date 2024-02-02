fbfac2643fac1869cd87509c387480b874ab172c
haderman <hadermania@gmail.com>
2024-02-02 14:11:34 -0500

    Refactor project structure and improve analytics logging
    - Remove @astrojs/partytown integration and related GTM configuration from astro.config.mjs and Layout.astro.
    - Remove @astrojs/partytown dependency from package.json.
    - Add data-name attributes to section elements in FAQs.astro, Features.astro, Hero.astro, and LastCallToAction.astro for improved analytics tracking.
    - Modify Accordion.astro to include the title attribute in the summary tag for better accessibility and tracking.
    - Replace GTM-based analytics with a custom logging solution using a new logger.js module in src/lib.
    - Implement event logging for page views, clicks, and other interactions within Layout.astro using the new logger function.

---
+++ src/lib/logger.js
@@ -0,0 +1,64 @@
+import { customAlphabet } from 'nanoid';
+
+export const nanoid = customAlphabet('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz');
+
+const isProd = import.meta.env.PROD;
+const isDev = import.meta.env.DEV;
+
+// This could be env variables in the future?
+const DATASET_NAME = 'alan-landing';
+const ENDPOINT = `https://api.axiom.co/v1/datasets/${DATASET_NAME}/ingest`;
+const TOKEN = 'xaat-3ae3ab60-5254-40bb-8a7a-7f8afd6e544b';
+
+function generateUniqueId() {
+  return `usr_${nanoid(22)}`;
+}
+
+function getUserId() {
+  let userId = localStorage.getItem('userId');
+  if (!userId) {
+    userId = generateUniqueId();
+    localStorage.setItem('userId', userId);
+  }
+  return userId;
+}
+
+/**
+ * Logs user interaction events to a remote server in development mode and prints the data to the console.
+ *
+ * @param {Object} data The event data to log. The structure depends on the event type:
+ * - For click events: `{ type: 'click', href: string }`
+ * - For hover events: `{ type: 'hover', element: string }`
+ * - For scroll events: `{ type: 'scroll', position: number }`
+ * - For page view events: `{ type: 'pageview', href: string }`
+ * - For session end events: `{ type: 'sessionend' }`
+ */
+export function logger(data) {
+  const userId = getUserId();
+  const enrichedData = [{
+    _time: new Date().toISOString(),
+    attributes: {
+      userId,
+    },
+    data,
+  }];
+
+  if (isProd) {
+    fetch(ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify(enrichedData),
+    }).then((response) => {
+      console.log('logger: ', response);
+    }).catch((error) => {
+      console.error('logger: ', error);
+    });
+  }
+
+  if (isDev) {
+    console.log('logger: ', enrichedData);
+  }
+}
