134a50020b2b75b0fd6320628fb510138652f805
haderman <hadermania@gmail.com>
2024-02-06 12:08:22 -0500

    Remove debug log from sendWelcomeMessage function in brevo.js

--- src/lib/brevo.js
+++ src/lib/brevo.js
@@ -2,7 +2,6 @@ const URL = 'https://api.brevo.com/v3/smtp/email';
 const API_KEY = import.meta.env.BREVO_API_KEY;
 
 export function sendWelcomeMessage(email) {
-  console.log('email: ', API_KEY);
   const data = {
     to: [{ email }],
     templateId: 3,
