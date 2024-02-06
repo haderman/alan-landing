ac94c069039cf682b8a0c5ae3c84306552d0d599
haderman <hadermania@gmail.com>
2024-02-06 00:58:11 -0500

    Update API key in brevo.js

--- src/lib/brevo.js
+++ src/lib/brevo.js
@@ -1,7 +1,8 @@
 const URL = 'https://api.brevo.com/v3/smtp/email';
-const API_KEY = import.meta.env.SUPABASE_URL;
+const API_KEY = import.meta.env.BREVO_API_KEY;
 
 export function sendWelcomeMessage(email) {
+  console.log('email: ', API_KEY);
   const data = {
     to: [{ email }],
     templateId: 3,
@@ -10,6 +11,7 @@ export function sendWelcomeMessage(email) {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
+      'accept': 'application/json',
       'api-key': API_KEY
     },
     body: JSON.stringify(data),
