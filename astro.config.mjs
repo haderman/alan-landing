07aeb193901941e3a90c73c7c203392ec1612682
haderman <hadermania@gmail.com>
2024-04-06 16:18:05 -0500

    Update Astro adapter from Vercel to Cloudflare and upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
 import icon from 'astro-icon';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    imageService: {
-      enabled: true
-    },
-  }),
+  adapter: cloudflare(),
   site: 'https://www.alanai.app/',
   integrations: [icon()]
 });
