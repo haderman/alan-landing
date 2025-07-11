548ac68eb542e8a928f9de904b877ebfd5824b6d
haderman <hadermania@gmail.com>
2025-07-10 22:47:49 -0500

    Migrate Microsoft Clarity analytics to use astro-clarity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
-
+import clarity from "@kbyte-tech/astro-clarity";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
@@ -16,5 +16,12 @@ export default defineConfig({
     },
   }),
   site: "https://www.alanai.app/",
-  integrations: [icon(), partytown()],
+  integrations: [
+    icon(),
+    partytown(),
+    clarity({
+      enabled: true,
+      projectId: "sd18gysdq6",
+    }),
+  ],
 });
